3a965e88ab11ea3a48d821bcd1567b049847197b
ganesh1603 <118152658+ganesh1603@users.noreply.github.com>
2023-07-07 01:08:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,20 +8,13 @@ THRESHOLD=0.5
 
 st.title("PADDY DEFICIENCY DETECTOR")
 
-
-
-
-
-
 select=st.selectbox("pick",["camera","upload image"])
 if select=="camera":
     cam=st.camera_input("capture a photo")
     if cam is not None:
         file_bytes = np.asarray(bytearray(cam.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
-        camera=camera(opencv_image)
-        pred=pred(camera)
-        iff(pred)
+        st.write("MASHAN ORU LOOSUKUDHI")
         st.image(opencv_image,channel="RGB")
 
 else:
@@ -29,10 +22,8 @@ else:
     if uploaded_file is not None:
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
         opencv_image1= cv2.imdecode(file_bytes, 1)
-        resized=file(opencv_image1)
-        pred=pred(resized)
-        iff(pred)
-        st.image(resized.astype(int),channels="BGR")
+        st.write("MASHAN ORU LOOSUKUDHI")
+        st.image(opencv_image1,channels="BGR")
 
 
 
