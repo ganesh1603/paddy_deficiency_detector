aaaa1279d381d7c3fe2fe16dd8645d7980aa919d
ganesh1603 <118152658+ganesh1603@users.noreply.github.com>
2023-07-07 02:01:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ import streamlit as st
 url = 'www.facebook.com'
 
 st.markdown(f'''
-<a href={url}><button style="background-color:GreenYellow;">Stackoverflow</button></a>
+<a href={url}><button style="background-color:GreenYellow;">LINKEDIN</button></a>
 ''',
 unsafe_allow_html=True)
 
