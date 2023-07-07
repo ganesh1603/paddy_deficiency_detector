2ad73c0c88faae029db244d5c8e6b0c3f58d5283
ganesh1603 <118152658+ganesh1603@users.noreply.github.com>
2023-07-07 12:20:34 +0530

    Update page1.py

--- page1.py
+++ page1.py
@@ -62,7 +62,7 @@ if select=="camera":
         camera=camera(opencv_image)
         pred=pred(camera)
         iff(pred)
-        st.image(opencv_image,channel="RGB")
+        st.image(opencv_image,channels="RGB")
 
 else:
     uploaded_file = st.file_uploader("Choose a image file", type=["jpg","png"])
